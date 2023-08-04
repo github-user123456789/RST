ef6e7795119fab279e561d02aec0990691844233
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2023-08-04 17:14:09 +0100

    Create punch_anim.lua

---
+++ punch_anim.lua
@@ -0,0 +1,81 @@
+return {
+		Properties = {
+			Looping = false,
+			Priority = Enum.AnimationPriority.Action
+		},
+		Keyframes = {
+			[0] = {
+				["HumanoidRootPart"] = {
+					["Torso"] = {
+						["Left Arm"] = {
+						},
+						["Right Arm"] = {
+						},
+						["Head"] = {
+						},
+					},
+				},
+			},
+			[0.15] = {
+				["HumanoidRootPart"] = {
+					["Torso"] = {
+						CFrame = CFrame.Angles(math.rad(-7.219), math.rad(9.683), math.rad(-52.884)),
+						["Left Arm"] = {
+							CFrame = CFrame.Angles(math.rad(-41.711), 0, 0),
+						},
+						["Right Arm"] = {
+							CFrame = CFrame.Angles(math.rad(30.08), math.rad(-44.748), math.rad(155.787)),
+						},
+						["Head"] = {
+							CFrame = CFrame.Angles(0, 0, math.rad(50.993)),
+						},
+					},
+				},
+			},
+			[0.35] = {
+				["HumanoidRootPart"] = {
+					["Torso"] = {
+						CFrame = CFrame.Angles(math.rad(9.397), math.rad(2.464), math.rad(24.752)),
+						["Left Arm"] = {
+							CFrame = CFrame.Angles(math.rad(-49.561), 0, math.rad(10.199)),
+						},
+						["Right Arm"] = {
+							CFrame = CFrame.Angles(math.rad(18.277), math.rad(-24.637), math.rad(99.465)),
+						},
+						["Head"] = {
+							CFrame = CFrame.Angles(0, 0, math.rad(-33.174)),
+						},
+					},
+				},
+			},
+			[0.5] = {
+				["HumanoidRootPart"] = {
+					["Torso"] = {
+						CFrame = CFrame.Angles(math.rad(9.397), math.rad(2.464), math.rad(70.818)),
+						["Left Arm"] = {
+							CFrame = CFrame.Angles(math.rad(-14.439), math.rad(5.901), math.rad(8.365)),
+						},
+						["Right Arm"] = {
+							CFrame = CFrame.Angles(math.rad(11.459), math.rad(18.22), math.rad(99.064)),
+						},
+					},
+				},
+			},
+			[0.65] = {
+				["HumanoidRootPart"] = {
+					["Torso"] = {
+						CFrame = CFrame.Angles(math.rad(9.397), math.rad(2.464), math.rad(70.818)),
+						["Left Arm"] = {
+							CFrame = CFrame.Angles(math.rad(-14.439), math.rad(5.901), math.rad(8.365)),
+						},
+						["Right Arm"] = {
+							CFrame = CFrame.Angles(math.rad(11.459), math.rad(18.22), math.rad(99.064)),
+						},
+						["Head"] = {
+							CFrame = CFrame.Angles(0, 0, math.rad(-33.174)),
+						},
+					},
+				},
+			},
+		}
+	}
