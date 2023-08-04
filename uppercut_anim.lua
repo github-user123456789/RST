3b83547eb62ca9ed083ec33d3cdb5d4c508f887e
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2023-08-04 18:18:56 +0100

    Create uppercut_anim.lua

---
+++ uppercut_anim.lua
@@ -0,0 +1,71 @@
+return {
+	Properties = {
+		Looping = false,
+		Priority = Enum.AnimationPriority.Action
+	},
+	Keyframes = {
+		[0] = {
+			["HumanoidRootPart"] = {
+				["Torso"] = {
+					["Right Arm"] = {
+						CFrame = CFrame.Angles(0, 0, math.rad(90.012)),
+					},
+					["Head"] = {
+					},
+				},
+			},
+		},
+		[0.1] = {
+			["HumanoidRootPart"] = {
+				["Torso"] = {
+					CFrame = CFrame.Angles(0, 0, math.rad(-36.669)),
+					["Right Arm"] = {
+						CFrame = CFrame.Angles(0, math.rad(-44.977), math.rad(44.977)),
+					},
+					["Head"] = {
+						CFrame = CFrame.Angles(0, 0, math.rad(18.678)),
+					},
+				},
+			},
+		},
+		[0.35] = {
+			["HumanoidRootPart"] = {
+				["Torso"] = {
+					CFrame = CFrame.Angles(0, 0, math.rad(-36.669)),
+					["Right Arm"] = {
+						CFrame = CFrame.Angles(0, math.rad(-44.977), math.rad(44.977)),
+					},
+					["Head"] = {
+						CFrame = CFrame.Angles(0, 0, math.rad(18.678)),
+					},
+				},
+			},
+		},
+		[0.5] = {
+			["HumanoidRootPart"] = {
+				["Torso"] = {
+					CFrame = CFrame.Angles(0, 0, math.rad(39.992)),
+					["Right Arm"] = {
+						CFrame = CFrame.new(0.15, 0.371, 0.009) * CFrame.Angles(math.rad(17.762), math.rad(34.95), math.rad(152.751)),
+					},
+					["Head"] = {
+						CFrame = CFrame.Angles(math.rad(-9.397), math.rad(3.38), math.rad(-39.706)),
+					},
+				},
+			},
+		},
+		[0.6] = {
+			["HumanoidRootPart"] = {
+				["Torso"] = {
+					CFrame = CFrame.Angles(0, 0, math.rad(39.992)),
+					["Right Arm"] = {
+						CFrame = CFrame.new(0.15, 0.371, 0.009) * CFrame.Angles(math.rad(17.762), math.rad(34.95), math.rad(152.751)),
+					},
+					["Head"] = {
+						CFrame = CFrame.Angles(math.rad(-9.397), math.rad(3.38), math.rad(-39.706)),
+					},
+				},
+			},
+		},
+	}
+}
